a1914fa52bf892fe056ac73c106ac38a104f3c1d
TanyaMashamba01 <mildredtanyamashamba01@gmail.com>
2025-05-01 23:08:18 +0200

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -14,26 +14,26 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     try {
         // Generate system status message
         const status = `
-╭──〔NEXUS-XMD〕───·๏
+╭──〔JOSHUAMAMBO1-XMD〕───·๏
 ┃🪀┃• *⏳ Uptime*:  ${runtime(process.uptime())} 
 ┃🪀┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}GB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}TB
 ┃🪀┃• *⚙️ HostName*: ${os.hostname()}
-┃🪀┃• *👨‍💻 Creator*: pkdriller 
+┃🪀┃• *👨‍💻 Creator*: MidKing1 
 ┃🪀┃• *🧬 Version*: 4.0.0
 ╰──────────────┈⊷
-> © NEXUS-𝐗𝐌𝐃`;
+> © JOSHUAMAMBO1-𝐗𝐌𝐃 is Alive`;
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://i.ibb.co/Kzbdr49w/nexus-xmd.jpg` },  
+            image: { url: `https://i.postimg.cc/4NdSqms8/MidKing.jpg` },  
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363288304618280@newsletter',
-                    newsletterName: 'Nexus 𝑿𝒎𝒅 🥳',
+                    newsletterJid: '120363347365643318@newsletter',
+                    newsletterName: ' 🫡MidKing🥳❤️‍🔥',
                     serverMessageId: 143
                 }
             }
